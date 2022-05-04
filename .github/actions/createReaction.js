9b0fb5725304c392b85f7487ff689b1c93df7c68
taenykim <kimtaeeeny@gmail.com>
2022-05-04 15:42:05 +0900

    add action

---
+++ .github/actions/createReaction.js
@@ -0,0 +1,10 @@
+const { getOctokit, context } = require("@actions/github");
+
+const octokit = getOctokit(process.env.GITHUB_TOKEN);
+
+octokit.rest.reactions.createForIssue({
+  owner: "taenykim",
+  repo: "action-npm-test",
+  issue_number: context.issue.number,
+  content: "heart",
+});
