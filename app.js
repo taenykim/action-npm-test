a00102932dedfb61c76662c79d6abfade10ca3cd
taeynkim <kimtaeeeny@gmail.com>
2022-05-27 12:34:41 +0900

    update action

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { execSync } = require("child_process");
 const execSyncToString = (param) => execSync(param).toString("utf8");
 
 console.log("start", process.env.TEST_SECRET);
+console.log("gh token", process.env.GH_TOKEN);
 
 const next = execSyncToString("npm run next");
 
