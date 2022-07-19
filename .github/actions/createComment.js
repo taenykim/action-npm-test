31e1dbb7bbb49813ba5b6c3d33619cfc82c07558
taeny-kim <taeny.kim@navercorp.com>
2022-07-19 15:46:37 +0900

    comment

--- .github/actions/createComment.js
+++ .github/actions/createComment.js
@@ -5,12 +5,13 @@ const octokit = getOctokit(process.env.GITHUB_TOKEN);
 (async function () {
   const action = context.payload.action;
 
-  const res = await octokit.rest.issues.listComments({
-    owner: "taenykim",
-    repo: "action-npm-test",
-    issue_number: context.issue.number,
-  });
-  const a = res.data
+  const en = (
+    await octokit.rest.issues.listComments({
+      owner: "taenykim",
+      repo: "action-npm-test",
+      issue_number: context.issue.number,
+    })
+  ).data
     .filter((item) => {
       return item.user.login === `github-actions[bot]`;
     })
@@ -19,8 +20,18 @@ const octokit = getOctokit(process.env.GITHUB_TOKEN);
       return item.body.includes(`### 데모`);
     });
 
-  console.log(a.length);
+  const demoComment = en[0];
+  const test = en[1];
+  console.log(test, "test");
+
+  octokit.rest.issues.updateComment({
+    owner: "taenykim",
+    repo: "action-npm-test",
+    comment_id: demoComment.id,
+    body: "hihi",
+  });
 })();
+
 // octokit.rest.issues.createComment({
 //   owner: "taenykim",
 //   repo: "action-npm-test",
