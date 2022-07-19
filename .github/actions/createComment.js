b29eae03a98d31a744b710c41a944fe02cc574e8
taeny-kim <taeny.kim@navercorp.com>
2022-07-19 15:03:13 +0900

    ef

---
+++ .github/actions/createComment.js
@@ -0,0 +1,12 @@
+const { getOctokit, context } = require("@actions/github");
+
+const octokit = getOctokit(process.env.GITHUB_TOKEN);
+
+const action = context.action;
+
+octokit.rest.issues.createComment({
+  owner: "taenykim",
+  repo: "action-npm-test",
+  issue_number: context.issue.number,
+  body: action,
+});
